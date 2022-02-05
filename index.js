a0d91a1546f3d0c4582e4d528620dfc38d60fc30
JMohajery <javidmohajery@gmail.com>
2022-02-05 11:54:21 +0330

    parse request url

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const http = require('http');
+var url = require('url');
 const server = http.createServer((req, res) => {
+
+    //Get the URL and parse it
+    var parsedUrl =  url.parse(req.url, true);
+
+    //Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace('/^\/+|\/+$/g','');
+
+    //return response
     res.end("hello world\n");
+
+    //Log the request path
+    console.log("request received on path:" + trimmedPath);
 })
 server.listen(3000, () => {
     console.log("server is listhening to port 3000");
