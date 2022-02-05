e2777705443850688e006368b3726c8b73f3f9f9
JMohajery <javidmohajery@gmail.com>
2022-02-05 12:19:04 +0330

    parse request method- fix bug trmming path

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const server = http.createServer((req, res) => {
 
     //Get the path
     var path = parsedUrl.pathname;
-    var trimmedPath = path.replace('/^\/+|\/+$/g','');
-
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+    //Get method
+    var method = req.method.toLowerCase();
     //return response
     res.end("hello world\n");
 
     //Log the request path
-    console.log("request received on path:" + trimmedPath);
+    console.log("request received on path: " + trimmedPath + " with method: " + method);
 })
 server.listen(3000, () => {
     console.log("server is listhening to port 3000");
