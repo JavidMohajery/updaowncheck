b68ac94ce038125f757a811596e38a06c25297c8
JMohajery <javidmohajery@gmail.com>
2022-02-10 21:07:02 +0330

    parse query strings- read header

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ const server = http.createServer((req, res) => {
     //Get the path
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    //Get query string
+    var queyStringObject = parsedUrl.query;
+
     //Get method
     var method = req.method.toLowerCase();
+
+    //Get headers
+    var headers = req.headers;
+
     //return response
     res.end("hello world\n");
 
     //Log the request path
-    console.log("request received on path: " + trimmedPath + " with method: " + method);
+    console.log("request received on path: " + trimmedPath + " with method: " + method + " with query string object: ", queyStringObject);
+    console.log('request header is ', headers);
 })
 server.listen(3000, () => {
     console.log("server is listhening to port 3000");
