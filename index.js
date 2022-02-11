4e52f568b7e23d58fdb98b1b9f07165407f02fd8
JMohajery <javidmohajery@gmail.com>
2022-02-11 10:47:36 +0330

    routing requests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+
 const server = http.createServer((req, res) => {
 
     //Get the URL and parse it
@@ -18,13 +20,55 @@ const server = http.createServer((req, res) => {
     //Get headers
     var headers = req.headers;
 
-    //return response
-    res.end("hello world\n");
+    // Get the Payload if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = "";
+    req.on("data", function(data){
+        buffer += decoder.write(data);  
+    });
 
-    //Log the request path
-    console.log("request received on path: " + trimmedPath + " with method: " + method + " with query string object: ", queyStringObject);
-    console.log('request header is ', headers);
+    req.on("end", function(){
+        buffer += decoder.end();
+
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        var data = {
+            'trimmedPath':trimmedPath,
+            'queyStringObject': queyStringObject,
+            'method':method,
+            'headers':headers,
+            'payload':buffer
+        }
+
+        chosenHandler(data, function(statusCode, payload){
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            payload = typeof(payload) =='object' ? payload : {};
+
+            var payloadString = JSON.stringify(payload);
+
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log("Returning this response", statusCode, payloadString);
+        });
+
+    })
+    
 })
 server.listen(3000, () => {
     console.log("server is listhening to port 3000");
 })
+
+var handlers = {}
+
+handlers.sample = function(data, callback){
+    callback(406, {'name':'sample handler'});
+}
+handlers.notFound = function(data, callback){
+    callback(404);
+}
+
+var router = {
+    'sample' : handlers.sample
+}
